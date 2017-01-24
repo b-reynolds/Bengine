be87fa7ecf2adcf129a12eaddeae75a165a3248c
Ben Reynolds <benr420@gmail.com>
2017-01-24 12:54:14 +0000

    Create Vector2D class

---
+++ Bengine/Bengine/Vector2D.h
@@ -0,0 +1,21 @@
+#pragma once
+
+template<class T>
+class Vector2D
+{
+
+public:
+
+	T x;
+	T y;
+
+	Vector2D() : x(0), y(0) {}
+	Vector2D(const T &x, const T &y) : x(x), y(y) {}
+
+	Vector2D& operator+(const Vector2D &v) { x += v.x; y += v.y; return *this; }
+	Vector2D& operator+=(const Vector2D& v) { x += v.x; y += v.y; return *this; }
+	Vector2D& operator-=(const Vector2D& v) { x -= v.x; y -= v.y; return *this; }
+	Vector2D& operator*=(const Vector2D& v) { x *= v.x; y *= v.y; return *this; }
+	Vector2D& operator/=(const Vector2D& v) { x /= v.x; y /= v.y; return *this; }
+
+};
