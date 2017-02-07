378f09f6f579e80cdd14892f948785308a13412b
Ben Reynolds <benr420@gmail.com>
2017-02-07 22:05:59 +0000

    Add SDL error messages to initialize error logs

--- Bengine/Bengine/Game.cpp
+++ Bengine/Bengine/Game.cpp
@@ -6,25 +6,25 @@
 
 bool Game::initialize()
 {
-	Logger* logger = Logger::getInstance();
+	BG::Logger* logger = BG::Logger::getInstance();
 
 	// Initialize the video subsystem
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
 		// Subsystem initialization failed, report error and exit.
-		logger->log(Logger::ERROR, "Failed to initialize video subsystem");
+		logger->log(BG::Logger::ERROR, std::string("Failed to initialize video subsystem (") + SDL_GetError() + ")");
 		SDL_Quit();
 		return false;
 	}
-	logger->log(Logger::INFO, "Initialized video subsystem");
+	logger->log(BG::Logger::INFO, "Initialized video subsystem");
 
 	// Initialize SDL_Image
 	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
-		logger->log(Logger::ERROR, "Failed to initialize SDL_Image");
+		logger->log(BG::Logger::ERROR, std::string("Failed to initialize SDL_Image (") + IMG_GetError() + ")");
 		SDL_Quit();
 		return false;
 	}
-	logger->log(Logger::INFO, "Initialized SDL_Image");
+	logger->log(BG::Logger::INFO, "Initialized SDL_Image");
 
 	// Create the window used for rendering
 	window = SDL_CreateWindow(WIN_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_OPENGL);
@@ -32,11 +32,11 @@ bool Game::initialize()
 	if (window == nullptr)
 	{
 		// Window creation failed, report error, clean up and exit.
-		logger->log(Logger::ERROR, "Failed to create window");
+		logger->log(BG::Logger::ERROR, std::string("Failed to create window (") + SDL_GetError() + ")");
 		SDL_Quit();
 		return false;
 	}
-	logger->log(Logger::INFO, "Created window");
+	logger->log(BG::Logger::INFO, "Created window");
 
 	// Create a renderer with hardware acceleration and vsync enabled
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
@@ -44,33 +44,17 @@ bool Game::initialize()
 	if (renderer == nullptr)
 	{
 		// Render creation failed, report error, clean up and exit.
-		logger->log(Logger::ERROR, "Failed to create renderer");
+		logger->log(BG::Logger::ERROR, std::string("Failed to create renderer") + SDL_GetError() + ")");
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 		return false;
 	}
-	logger->log(Logger::INFO, "Created renderer");
+	logger->log(BG::Logger::INFO, "Created renderer");
 
-	auto resourceManager = ResourceManager::getInstance();
+	auto resourceManager = BG::ResourceManager::getInstance();
 
 	txtrBackground = resourceManager->getTexture("Images/tile.bmp", renderer);
-	if(txtrBackground == nullptr)
-	{
-		SDL_DestroyTexture(txtrBackground);
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return false;
-	}
-
 	txtrLogo = resourceManager->getTexture("Images/Icon/bengine.fw.png", renderer);
-		
-	if (txtrLogo == nullptr)
-	{
-		SDL_DestroyTexture(txtrBackground);
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return false;
-	}
 
 	return true;
 }
@@ -82,7 +66,7 @@ bool Game::run()
 	{
 		if (!update())
 		{
-			Logger::getInstance()->log(Logger::INFO, "Exiting application");
+			BG::Logger::getInstance()->log(BG::Logger::INFO, "Exiting application");
 			SDL_DestroyTexture(txtrBackground);
 			SDL_DestroyTexture(txtrLogo);
 			SDL_DestroyRenderer(renderer);
@@ -129,24 +113,24 @@ void Game::draw()
 	// Clear the renderer
 	SDL_RenderClear(renderer);
 
-	auto tileSize = Vector2D<int>();
+	auto tileSize = BG::Vector2i();
 	SDL_QueryTexture(txtrBackground, nullptr, nullptr, &tileSize.x, &tileSize.y);
 
-	auto tiles = Vector2D<int>(WIN_WIDTH / tileSize.x, WIN_HEIGHT / tileSize.y);
+	auto tiles = BG::Vector2i(WIN_WIDTH / tileSize.x, WIN_HEIGHT / tileSize.y);
 
 	for (int x = 0; x < tiles.x; ++x)
 	{
 		for (int y = 0; y < tiles.y; ++y)  
 		{
-			Renderer::renderTexture(txtrBackground, renderer, x * tileSize.x, y * tileSize.y);
+			BG::Renderer::renderTexture(txtrBackground, renderer, x * tileSize.x, y * tileSize.y);
 		}
 	}
 
-	auto logoSize = Vector2D<int>();
+	auto logoSize = BG::Vector2i();
 	
 	SDL_QueryTexture(txtrLogo, nullptr, nullptr, &logoSize.x, &logoSize.y);
 
-	Renderer::renderTexture(txtrLogo, renderer, WIN_WIDTH / 2 - logoSize.x / 2, WIN_HEIGHT / 2 - logoSize.y / 2, logoSize.x, logoSize.y );
+	BG::Renderer::renderTexture(txtrLogo, renderer, WIN_WIDTH / 2 - logoSize.x / 2, WIN_HEIGHT / 2 - logoSize.y / 2, logoSize.x, logoSize.y );
 
 	// Update the screen
 	SDL_RenderPresent(renderer);
