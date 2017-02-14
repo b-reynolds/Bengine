eca674fdd7ab01eda378c9c117905af1d9878e6b
Ben Reynolds <benr420@gmail.com>
2017-02-14 23:43:21 +0000

    Change style/refactor

--- Bengine/Bengine/Audio.cpp
+++ Bengine/Bengine/Audio.cpp
@@ -1,19 +1,27 @@
 #include "Audio.h"
 
-/**
-* @brief Play a sound effect file
-* @param soundEffect The sound effect file to play
-* @param loops The amount of times to loop the sound effect (None: 0, Infinite: -1)
-* @param fadeInMilliseconds The amount of time in milliseconds to fade the sound effect in with (No Fade: 0)
+/*\
+* \brief Plays a sound effect file
+* \param soundEffect sound effect file to play
+* \param loops amount of times to loop the sound effect (None: 0, Infinite: -1)
+* \param fadeInMilliseconds amount of time in milliseconds to fade the sound effect in with (No Fade: 0)
 */
-void BG::Audio::playSoundEffect(Mix_Chunk* soundEffect, const int& channel, const int& loops, const int &fadeInMilliseconds)
+void BG::Audio::playSoundEffect(SoundEffect* soundEffect, const int& channel, const int& loops, const int &fadeInMilliseconds)
 {
 	Mix_FadeInChannel(channel, soundEffect, loops, fadeInMilliseconds);
 }
 
 /**
-* @brief Stop all currently playing sound effects on the specified channel
-* @param fadeOutMilliseconds The amount of time in milliseconds to fade the music out with (No Fade: 0)
+* \brief Returns true if the specified channel is currently playing (ignores volume and pause state)
+*/
+bool BG::Audio::isChannelPlaying(const int& channel)
+{
+	return Mix_Playing(channel);
+}
+
+/**
+* \brief Stop all currently playing sound effects on the specified channel
+* \param fadeOutMilliseconds amount of time in milliseconds to fade the music out with (No Fade: 0)
 */
 void BG::Audio::stopChannel(const int& channel, const int &fadeOutMilliseconds)
 {
@@ -21,35 +29,28 @@ void BG::Audio::stopChannel(const int& channel, const int &fadeOutMilliseconds)
 }
 
 /**
-* @brief Play a music file
-* @param music The music file to play
-* @param loops The amount of times to loop the music (None: 0, Infinite: -1)
-* @param fadeInMilliseconds The amount of time in milliseconds to fade the music in with (No Fade: 0)
+* \brief Set the pause state of the specified channel
+* \param state pause state
 */
-void BG::Audio::playMusic(Mix_Music* music, const int& loops, const int& fadeInMilliseconds)
+void BG::Audio::setChannelPaused(const int &channel, const bool &state)
 {
-	Mix_FadeInMusic(music, loops, fadeInMilliseconds);
-}
-
-/**
-* @brief Stop all currently playing music
-* @param fadeOutMilliseconds The amount of time in milliseconds to fade the music out with (No Fade: 0)
-*/
-void BG::Audio::stopMusic(const int &fadeOutMilliseconds)
-{
-	if (fadeOutMilliseconds > 0)
+	if (isChannelPlaying(channel))
 	{
-		Mix_FadeOutMusic(fadeOutMilliseconds);
-	}
-	else
-	{
-		Mix_HaltMusic();
+		state ? Mix_Pause(channel) : Mix_Resume(channel);
 	}
 }
 
 /**
-* @brief Set the volume level sound effect files are played at on the specified channel (0 - 128)
-* @param volume Desired volume level
+* \brief Returns true if the specified channel is currently paused
+*/
+bool BG::Audio::getChannelPaused(const int& channel)
+{
+	return Mix_Paused(channel);
+}
+
+/**
+* \brief Set the volume level sound effect files are played at on the specified channel (0 - 128)
+* \param volume desired volume level
 */
 void BG::Audio::setChannelVolume(const int& channel, int volume)
 {
@@ -65,8 +66,71 @@ void BG::Audio::setChannelVolume(const int& channel, int volume)
 }
 
 /**
-* @brief Set the volume level music files are played at (0 - 128)
-* @param volume Desired volume level
+* \brief Returns the current volume level of the specified channel
+*/
+int BG::Audio::getChannelVolume(const int& channel)
+{
+	return Mix_Volume(channel, -1);
+}
+
+/**
+* \brief Plays a music file
+* \param music music file to play
+* \param loops amount of times to loop the music (None: 0, Infinite: -1)
+* \param fadeInMilliseconds amount of time in milliseconds to fade the music in with (No Fade: 0)
+*/
+void BG::Audio::playMusic(Music* music, const int& loops, const int& fadeInMilliseconds)
+{
+	Mix_FadeInMusic(music, loops, fadeInMilliseconds);
+}
+
+/**
+* \brief Returns true if music is currently being played (ignores volume and pause state)
+*/
+bool BG::Audio::isMusicPlaying()
+{
+	return Mix_PlayingMusic();
+}
+
+/**
+* \brief Stops all currently playing music
+* \param fadeOutMilliseconds amount of time in milliseconds to fade the music out with (No Fade: 0)
+*/
+void BG::Audio::stopMusic(const int &fadeOutMilliseconds)
+{
+	if (fadeOutMilliseconds > 0)
+	{
+		Mix_FadeOutMusic(fadeOutMilliseconds);
+	}
+	else
+	{
+		Mix_HaltMusic();
+	}
+}
+
+/**
+* \brief Set the pause state of currently playing music
+* \param state pause state
+*/
+void BG::Audio::setMusicPaused(const bool& state)
+{
+	if (isMusicPlaying())
+	{
+		state ? Mix_PauseMusic() : Mix_ResumeMusic();
+	}
+}
+
+/**
+* \brief Returns true if music is currently paused
+*/
+bool BG::Audio::getMusicPaused()
+{
+	return Mix_PausedMusic();
+}
+
+/**
+* \brief Set the volume level music files are played at (0 - 128)
+* \param volume desired volume level
 */
 void BG::Audio::setMusicVolume(int volume)
 {
@@ -82,71 +146,7 @@ void BG::Audio::setMusicVolume(int volume)
 }
 
 /**
-* @brief Set the pause state of the specified channel
-* @param state Paused: True, Playing: False
-*/
-void BG::Audio::setChannelPaused(const int &channel, const bool &state)
-{
-	if(isChannelPlaying(channel))
-	{
-		state ? Mix_Pause(channel) : Mix_Resume(channel);
-	}
-}
-
-/**
-* @brief Set the pause state of currently playing music
-* @param state Paused: True, Playing: False
-*/
-void BG::Audio::setMusicPaused(const bool& state)
-{
-	if (isMusicPlaying())
-	{
-		state ? Mix_PauseMusic() : Mix_ResumeMusic();
-	}
-}
-
-/**
-* @brief Returns true if the specified channel is currently playing (ignores volume and pause state)
-*/
-bool BG::Audio::isChannelPlaying(const int& channel)
-{
-	return Mix_Playing(channel);
-}
-
-/**
-* @brief Returns true if music is currently being played (ignores volume and pause state)
-*/
-bool BG::Audio::isMusicPlaying()
-{
-	return Mix_PlayingMusic();
-}
-
-/**
-* @brief Returns true if the specified channel is currently paused
-*/
-bool BG::Audio::isChannelPaused(const int& channel)
-{
-	return Mix_Paused(channel);
-}
-
-/**
-* @brief Returns true if music is currently paused
-*/
-bool BG::Audio::isMusicPaused()
-{
-	return Mix_PausedMusic();
-}
-
-/**
-* @brief Returns the current volume level of the specified channel
-*/
-int BG::Audio::getChannelVolume(const int& channel)
-{
-	return Mix_Volume(channel, -1);
-}
-
-/**
-* @brief Returns the current music volume level
+* \brief Returns the current music volume level
 */
 int BG::Audio::getMusicVolume()
 {
--- Bengine/Bengine/Audio.h
+++ Bengine/Bengine/Audio.h
@@ -1,32 +1,57 @@
 #pragma once
 #include <SDL_mixer.h>
+#include "Bengine.h"
 
 namespace BG
 {
+	/* \brief Audio Manager Class
+	 * Handles the playing and manipulation of Music and SoundEffect files
+	 */
 	class Audio
 	{
 
 	public:
 
-		static void playSoundEffect(Mix_Chunk* soundEffect, const int &channel, const int &loops, const int &fadeInMilliseconds);
+		/* Plays a sound effect file */
+		static void playSoundEffect(SoundEffect* soundEffect, const int &channel, const int &loops, const int &fadeInMilliseconds);
+
+		/* Returns true if the specified channel is currently playing (ignores volume and pause state) */
+		static bool isChannelPlaying(const int &channel);
+
+		/* Stop all currently playing sound effects on the specified channel */
 		static void stopChannel(const int &channel, const int &fadeOutMilliseconds);
 
-		static void playMusic(Mix_Music* music, const int &loops, const int &fadeInMilliseconds);
-		static void stopMusic(const int &fadeOutMilliseconds);
-
+		/* Set the pause state of the specified channel */
 		static void setChannelPaused(const int &channel, const bool &state);
-		static void setMusicPaused(const bool &state);
 
+		/* Returns true if the specified channel is currently paused */
+		static bool getChannelPaused(const int &channel);
+
+		/* Set the volume level sound effect files are played at on the specified channel (0 - 128) */
 		static void setChannelVolume(const int &channel, int volume);
-		static void setMusicVolume(int volume);
 
-		static bool isChannelPlaying(const int &channel);
+		/* Returns the current volume level of the specified channel */
+		static int getChannelVolume(const int &channel);
+
+		/* Plays a music file */
+		static void playMusic(Music* music, const int &loops, const int &fadeInMilliseconds);
+
+		/* Returns true if music is currently being played (ignores volume and pause state) */
 		static bool isMusicPlaying();
 
-		static bool isChannelPaused(const int &channel);
-		static bool isMusicPaused();
+		/* Stops all currently playing music */
+		static void stopMusic(const int &fadeOutMilliseconds);
 
-		static int getChannelVolume(const int &channel);
+		/* Set the pause state of currently playing music */
+		static void setMusicPaused(const bool &state);
+
+		/* Returns true if music is currently paused */
+		static bool getMusicPaused();
+
+		/* Set the volume level music files are played at (0 - 128) */
+		static void setMusicVolume(int volume);
+
+		/* Returns the current music volume level */
 		static int getMusicVolume();
 
 	};
