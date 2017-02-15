6865be0677a93e2a3b93dfc1d3f23f13025eed29
Ben Reynolds <benr420@gmail.com>
2017-02-15 20:57:31 +0000

    Add comments to Transform class

--- Bengine/Bengine/Transform.cpp
+++ Bengine/Bengine/Transform.cpp
@@ -1,41 +1,70 @@
 #include "Transform.h"
 
+/*
+ * \brief Default Constructor
+ */
 BG::Transform::Transform()
 {
 	rotation = 0.0f;
 }
 
+/*
+ * \brief Initializes the Transform at the specified position
+ * \param position position
+ */
 BG::Transform::Transform(const Vector2f& position)
 {
 	this->position = position;
 	rotation = 0.0f;
 }
 
+/*
+ * \brief Set the Transform's position
+ * \param position position
+ */
 void BG::Transform::setPosition(const Vector2f& position)
 {
 	this->position = position;
 }
 
+/*
+ * \brief Returns the Transform's current position
+ */
 BG::Vector2f BG::Transform::getPosition() const
 {
 	return position;
 }
 
+/*
+ * \brief Move the Transform by the specified velocity
+ * \param velocity movement velocity
+ */
 void BG::Transform::move(const Vector2f& velocity)
 {
 	position += velocity;
 }
 
+/*
+ * \brief Set the Transform's rotation (degrees)
+ * \param angle angle (degrees)
+ */
 void BG::Transform::setRotation(const float& angle)
 {
 	rotation = angle;
 }
 
+/*
+ * \brief Returns the Transform's current rotation
+ */
 float BG::Transform::getRotation() const
 {
 	return rotation;
 }
 
+/*
+ * \brief Rotate the Transform by the specified angle (degrees)
+ * \brief angle angle (degrees)
+ */
 void BG::Transform::rotate(const float& angle)
 {
 	rotation += angle;
--- Bengine/Bengine/Transform.h
+++ Bengine/Bengine/Transform.h
@@ -3,28 +3,44 @@
 
 namespace BG
 {
-
+	/*
+	 * Transformation Class
+	 */
 	class Transform
 	{
 
 	public:
 
+		/* Default Constructor */
 		Transform();
+
+		/* Initializes the Transform at the specified position */
 		explicit Transform(const Vector2f &position);
 
+		/* Set the Transform's position */
 		void setPosition(const Vector2f &position);
+
+		/* Returns the Transform's current position */
 		Vector2f getPosition() const;
 
+		/* Move the Transform by the specified velocity */
 		void move(const Vector2f &velocity);
 
+		/* Set the Transform's rotation (degrees) */
 		void setRotation(const float &angle);
+
+		/* Returns the Transform's current rotation */
 		float getRotation() const;
 
+		/* Rotate the Transform by the specified angle (degrees) */
 		void rotate(const float &angle);
 
 	private:
 
+		/* Current position of the Transform */
 		Vector2f position;
+
+		/* Current rotation of the Transform in degrees */
 		float rotation;
 
 	};
