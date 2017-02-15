4110117ade6ccf870ca784e6f80f9484afd5d95d
Ben Reynolds <benr420@gmail.com>
2017-02-15 20:19:48 +0000

    Add comments to Window class

--- Bengine/Bengine/Window.cpp
+++ Bengine/Bengine/Window.cpp
@@ -3,6 +3,11 @@
 #include "GameObject.h"
 #include "Colour.h"
 
+/*
+ * \brief Creates and initializes a Window with the specified title and of the specified size
+ * \param title Window title
+ * \param size Window size
+ */
 BG::Window::Window(const std::string& title, const Vector2u& size)
 {
 	window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, size.x, size.y, SDL_WINDOW_OPENGL);
@@ -17,7 +22,6 @@ BG::Window::Window(const std::string& title, const Vector2u& size)
 	}
 	logger.log(BG::Logger::INFO, "Created window");
 
-
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
 	if(renderer == nullptr)
@@ -29,16 +33,25 @@ BG::Window::Window(const std::string& title, const Vector2u& size)
 	logger.log(BG::Logger::INFO, "Created renderer");
 }
 
+/*
+ * \brief Clears the Window's renderer ignoring the viewport and clip rectangle
+ */
 void BG::Window::clear() const
 {
 	SDL_RenderClear(renderer);
 }
 
+/*
+ **	\brief Updates the Window's contents post rendering
+ */
 void BG::Window::display() const
 {
 	SDL_RenderPresent(renderer);
 }
 
+/*
+ * \brief Destroys the Window and renderer freeing all associated memory
+ */
 void BG::Window::destroy()
 {
 	auto logger = Logger::getInstance();
@@ -50,6 +63,10 @@ void BG::Window::destroy()
 	renderer = nullptr;
 }
 
+/*
+ * \brief Render a GameObject to the Window
+ * \param gameObject GameObject to render
+ */
 void BG::Window::draw(GameObject& gameObject) const
 {
 	Transform* transform = gameObject.getTransform();
@@ -68,6 +85,11 @@ void BG::Window::draw(GameObject& gameObject) const
 	SDL_RenderCopyEx(renderer, texture, nullptr, &destination, angle, &origin, flipped);
 }
 
+/*
+ * \brief Renders a GameObject to the Window tinted with the specified Colour
+ * \param gameObject GameObject to render
+ * \param tint tint Colour
+ */
 void BG::Window::draw(GameObject& gameObject, const Colour& tint) const
 {
 	SDL_SetTextureColorMod(gameObject.getSprite()->getTexture(), tint.r, tint.g, tint.b);
@@ -75,6 +97,11 @@ void BG::Window::draw(GameObject& gameObject, const Colour& tint) const
 	SDL_SetTextureColorMod(gameObject.getSprite()->getTexture(), 255, 255, 255);
 }
 
+/*
+ * \brief Renderers a FloatRect of the specified Colour to the Window 
+ * \param rect FloatRect to render
+ * \param colour rectangle colour
+ */
 void BG::Window::draw(const FloatRect& rect, const Colour& colour) const
 {
 	SDL_Rect bounds = { rect.left, rect.top, rect.width, rect.height };
@@ -83,11 +110,18 @@ void BG::Window::draw(const FloatRect& rect, const Colour& colour) const
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 }
 
+/*
+ * \brief Set the Window's position
+ * \param position Window position
+ */
 void BG::Window::setPosition(const Vector2i& position) const
 {
 	SDL_SetWindowPosition(window, position.x, position.y);
 }
 
+/*
+ * Returns the Window's current position
+ */
 BG::Vector2i BG::Window::getPosition() const
 {
 	Vector2i position = Vector2i(0, 0);
@@ -95,11 +129,18 @@ BG::Vector2i BG::Window::getPosition() const
 	return position;
 }
 
+/*
+ * \brief Set the Window's size
+ * \param size Window size
+ */
 void BG::Window::setSize(const Vector2i& size) const
 {
 	SDL_SetWindowSize(window, size.x, size.y);
 }
 
+/*
+ * \brief Returns the Window's current size
+ */
 BG::Vector2i BG::Window::getSize() const
 {
 	Vector2i size = Vector2i(0, 0);
@@ -107,21 +148,35 @@ BG::Vector2i BG::Window::getSize() const
 	return size;
 }
 
+/*
+ * \brief Set the fullscreen state of the Window
+ * \brief state fullscreen state
+ */
 void BG::Window::setFullscreen(const bool& state) const
 {
 	SDL_SetWindowFullscreen(window, state ? SDL_WINDOW_FULLSCREEN : 0);
 }
 
+/*
+ * \brief Set the Window's title
+ * \param title Window title
+ */
 void BG::Window::setTitle(const std::string& title) const
 {
 	SDL_SetWindowTitle(window, title.c_str());
 }
 
+/*
+ * \brief Returns a reference to the Window's renderer
+ */
 SDL_Renderer* BG::Window::getRenderer() const
 {
 	return renderer;
 }
 
+/*
+ * \brief Returns true if the Window and its renderer initialized successfully
+ */
 bool BG::Window::isOpen() const
 {
 	return window != nullptr && renderer != nullptr;
--- Bengine/Bengine/Window.h
+++ Bengine/Bengine/Window.h
@@ -1,46 +1,75 @@
 #pragma once
-#include <SDL.h>
 #include "Vector2.h"
-#include <string>
 #include "Rectangle.h"
+#include <SDL.h>
+#include <string>
 
 namespace BG
 {
+	// Forward Declarations
 	struct Colour;
 	class GameObject;
 
+	/*
+	 * \brief Windowing Class
+	 * A wrapper for SDL_Window
+	 */
 	class Window
 	{
 
 	public:
 
+		/* Creates and initializes a window with the specified title and of the specified size */
 		Window(const std::string &title, const Vector2u &size);
 
+		/* Clears the Window's renderer ignoring the viewport and clip rectangle */
 		void clear() const;
+
+		/* Updates the windows contents post rendering */
 		void display() const;
+
+		/* Destroys the window and renderer freeing all associated memory */
 		void destroy();
 		
+		/* Render a GameObject to the window */
 		void draw(GameObject &gameObject) const;
+
+		/* Render a GameObject to the window tinted with the specified Colour */
 		void draw(GameObject &gameObject, const Colour &tint) const;
+
+		/* Render a FloatRect of the specified Colour to the window */
 		void draw(const FloatRect &rect, const Colour &colour) const;
 
+		/* Set the Window's position */
 		void setPosition(const Vector2i &position) const;
+
+		/* Returns the Window's current position */
 		Vector2i getPosition() const;
 
+		/* Set the Window's size */
 		void setSize(const Vector2i &size) const;
+
+		/* Returns the Window's current size */
 		Vector2i getSize() const; 
 
+		/* Set the fullscreen state of the Window */
 		void setFullscreen(const bool &state) const;
+
+		/* Set the Window's title */
 		void setTitle(const std::string &title) const;
 
+		/* Returns a reference to the Window's renderer */
 		SDL_Renderer* getRenderer() const;
 
-
+		/* Returns true if the Window and its renderer initialized successfully */
 		bool isOpen() const;
 
 	private:
 
+		/* The window that is rendered to */
 		SDL_Window* window;
+
+		/* Used for drawing/rendering to the window */
 		SDL_Renderer* renderer;
 
 	};
