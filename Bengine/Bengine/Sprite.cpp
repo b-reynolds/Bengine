d3abb3889c3faf808ad70511e1f34b40fe9a41bb
Ben Reynolds <benr420@gmail.com>
2017-02-15 21:15:06 +0000

    Add comments to Sprite class

--- Bengine/Bengine/Sprite.cpp
+++ Bengine/Bengine/Sprite.cpp
@@ -1,13 +1,20 @@
 #include "Sprite.h"
 #include "Window.h"
 
+/*
+ * \brief Default Constructor
+ */
 BG::Sprite::Sprite()
 {
+	flipped = SDL_FLIP_NONE;
 	texture = nullptr;
 	renderer = nullptr;
 }
+/*
+ * \brief Initialize the Sprite's Texture and Window
+ */
 
-BG::Sprite::Sprite(SDL_Texture* texture, Window* window)
+BG::Sprite::Sprite(Texture* texture, Window* window)
 {
 	this->texture = texture;
 	this->renderer = window->getRenderer();
@@ -19,52 +26,84 @@ BG::Sprite::Sprite(SDL_Texture* texture, Window* window)
 	size = Vector2f(sizeX, sizeY);
 }
 
+/*
+ * \brief Set the size of the Sprite
+ * \param size Sprite size
+ */
 void BG::Sprite::setSize(const Vector2f& size)
 {
 	this->size = size;
 }
 
-void BG::Sprite::setFlipped(const bool& state)
+/*
+* \brief Returns the Sprite's current size (scaled)
+*/
+BG::Vector2f BG::Sprite::getSize() const
 {
-	flipped = state ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE;
+	return Vector2f(size.x * scale.x, size.y * scale.y);
 }
-
+/*
+* \brief Set the Sprite's origin for rendering and rotation
+* \param origin Sprite origin
+*/
 void BG::Sprite::setOrigin(const Vector2f& origin)
 {
 	this->origin = origin;
 }
 
-SDL_Texture* BG::Sprite::getTexture() const
-{
-	return texture;
-}
-
-SDL_Renderer* BG::Sprite::getRenderer() const
-{
-	return renderer;
-}
-
-BG::Vector2f BG::Sprite::getSize() const
-{
-	return Vector2f(size.x * scale.x, size.y * scale.y);
-}
-
-SDL_RendererFlip BG::Sprite::getFlipped() const
-{
-	return flipped;
-}
-
+/*
+* \brief Returns the Sprite's current point of origin for rendering and rotation
+*/
 BG::Vector2f BG::Sprite::getOrigin() const
 {
 	return origin;
 }
 
+/*
+* \brief Set the Sprite's scale
+*/
 void BG::Sprite::setScale(const Vector2f& scale)
 {
 	this->scale = scale;
 }
 
+/*
+* \brief Returns the Sprite's current scale
+*/
 BG::Vector2f BG::Sprite::getScale() const
 {
 	return scale;
+}
+
+/* 
+ * \brief Set the horizontal flip state of the Sprite
+ * \param state flip state
+ */
+void BG::Sprite::setFlipped(const bool& state)
+{
+	flipped = state ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE;
+}
+
+/*
+* \brief Returns the horizontal flip state of the Sprite
+*/
+SDL_RendererFlip BG::Sprite::getFlipped() const
+{
+	return flipped;
+}
+
+/*
+ * \brief Returns a pointer to the Sprite's texture
+ */
+SDL_Texture* BG::Sprite::getTexture() const
+{
+	return texture;
+}
+
+/*
+ * \brief Returns a pointer to the Sprite's renderer
+ */
+SDL_Renderer* BG::Sprite::getRenderer() const
+{
+	return renderer;
 }
--- Bengine/Bengine/Sprite.h
+++ Bengine/Bengine/Sprite.h
@@ -1,40 +1,75 @@
 #pragma once
 #include <SDL.h>
 #include "Rectangle.h"
+#include "BengineTypedefs.h"
 
 namespace BG
 {
+	// Forward Declaration
 	class Window;
 
+	/*
+	 * \brief Sprite Class
+	 */
 	class Sprite
 	{
 
 	public:
 
+		/* Default Constructor */
 		Sprite();
-		Sprite(SDL_Texture* texture, Window* window);
 
+		/* Initialize the Sprite's Texture and Window */
+		Sprite(Texture* texture, Window* window);
+
+		/* Set the size of the Sprite */
 		void setSize(const Vector2f &size);
-		void setFlipped(const bool &state);
+
+		/* Returns the Sprite's current size (scaled) */
+		Vector2f getSize() const;
+
+		/* Set the Sprite's origin for rendering and rotation */
 		void setOrigin(const Vector2f &origin);
 
-
-		SDL_Texture* getTexture() const;
-		SDL_Renderer* getRenderer() const;
-		Vector2f getSize() const;
-		SDL_RendererFlip getFlipped() const;
+		/* Returns the Sprite's current point of origin for rendering and rotation */
 		Vector2f getOrigin() const;
 
+		/* Set the Sprite's scale */
 		void setScale(const Vector2f &scale);
+
+		/* Returns the Sprite's current scale */
 		Vector2f getScale() const;
+
+		/* Set the horizontal flip state of the Sprite */
+		void setFlipped(const bool &state);
+
+		/* Returns the horizontal flip state of the Sprite */
+		SDL_RendererFlip getFlipped() const;
+
+		/* Returns a pointer to the Sprite's texture */
+		SDL_Texture* getTexture() const;
+
+		/* Returns a pointer to the Sprite's renderer */
+		SDL_Renderer* getRenderer() const;
 
 	private:
 
+		/* Texture used when the Sprite is rendered */
 		SDL_Texture* texture;
+
+		/* Renderer used to render the Sprite */
 		SDL_Renderer* renderer;
+
+		/* Horizontal flip state of the Sprite*/
 		SDL_RendererFlip flipped;
+
+		/* Sprite's size without scaling factored */
 		Vector2f size;
+
+		/* Sprite's scale */
 		Vector2f scale;
+
+		/* Sprite's point of origin */
 		Vector2f origin;
 
 	};
