32638ef563cb2868f82712bc5d900697c48d40ab
Ben Reynolds <benr420@gmail.com>
2017-02-16 01:47:32 +0000

    Add comments to / expand Text class

--- Bengine/Bengine/Text.cpp
+++ Bengine/Bengine/Text.cpp
@@ -1,28 +1,104 @@
 #include "Text.h"
-#include "Colour.h"
 #include "Window.h"
 
+/*
+ * \brief Default Constructor
+ */
 BG::Text::Text()
 {
+	font = nullptr;
+	window = nullptr;
 }
 
-BG::Text::Text(Font* font, const std::string& text, const Colour &colour, Window* window)
+/*
+ * \brief Initialize the Text
+ * \param font font file to use for rendering
+ * \param text text to be displayed
+ * \param window window to render to
+ */
+BG::Text::Text(Font* font, const std::string& text, Window* window)
 {
 	this->font = font;
 	this->text = text;
-
-	this->colour.r = colour.r;
-	this->colour.g = colour.g;
-	this->colour.b = colour.b;
-	this->colour.a = colour.a;
-
 	this->window = window;
 }
 
+/*
+ * \brief Set the text that is displayed
+ * \brief text text to be displayed
+ */
+void BG::Text::setText(const std::string& text)
+{
+	this->text = text;
+}
+
+/*
+ * \brief Returns the current text value
+ */
+std::string BG::Text::getText() const
+{
+	return text;
+}
+
+/*
+ * \brief Set the text Colour
+ * \param colour text Colour
+ */
+void BG::Text::setColour(const Colour& colour)
+{
+	this->colour = colour;
+}
+
+/*
+ * \brief Returns the text Colour
+ */
+BG::Colour BG::Text::getColour() const
+{
+	return Colour(colour.r, colour.g, colour.b, colour.a);
+}
+
+/* 
+ * \brief Set the Text's origin for rendering and rotations
+ * \param origin point of origin
+ */
+void BG::Text::setOrigin(const Vector2f& origin)
+{
+	this->origin = origin;
+}
+
+/*
+ * \brief Returns the Text's current point of origin for rendering and rotations
+ */
+BG::Vector2f BG::Text::getOrigin() const
+{
+	return origin;
+}
+
+/*
+ * Creates and returns a texture ready for rendering 
+ */
 BG::Texture* BG::Text::getTexture() const
 {
-	SDL_Surface* surface = TTF_RenderText_Blended(font, text.c_str(), colour);
+	SDL_Color txtColour;
+
+	txtColour.r = colour.r;
+	txtColour.g = colour.g;
+	txtColour.b = colour.b;
+	txtColour.a = colour.a;
+
+	SDL_Surface* surface = TTF_RenderText_Blended(font, text.c_str(), txtColour);
+
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(window->getRenderer(), surface);
+
 	SDL_FreeSurface(surface);
+
 	return texture;
+}
+
+/*
+ * Returns a reference to the Text's Transform
+ */
+BG::Transform& BG::Text::getTransform() 
+{
+	return transform;
 }
--- Bengine/Bengine/Text.h
+++ Bengine/Bengine/Text.h
@@ -1,27 +1,71 @@
 #pragma once
 #include "Audio.h"
+#include "Transform.h"
+#include "Colour.h"
 #include <string>
 
 namespace BG
 {
+	// Forward Declaration
 	class Window;
-	struct Colour;
 
+	/*
+	 * Text Class
+	 */
 	class Text
 	{
 
 	public:
 
+		/* Default Constructor */
 		Text();
-		explicit Text(Font* font, const std::string &text, const Colour &colour, Window* window);
+
+		/* Initializing Constructor */
+		explicit Text(Font* font, const std::string &text, Window* window);
+
+		/* Set the text that is displayed */
+		void setText(const std::string &text);
 		
+		/* Returns the current text value */
+		std::string getText() const;
+
+		/* Set the text colour */
+		void setColour(const Colour &colour);
+		
+		/* Returns the text colour*/
+		Colour getColour() const;
+
+		/* Set the Text's origin for rendering and rotation */
+		void setOrigin(const Vector2f &origin);
+
+		/* Returns the Text's current point of origin for rendering and rotations */
+		Vector2f getOrigin() const;
+		
+		/* Creates and returns a texture ready for tendering */
 		Texture* getTexture() const;
 
+		/* Returns a reference to the Text's Transform */
+		Transform& getTransform();
 
+	private:
+
+		/* Text's current transformation */
+		Transform transform;
+
+		/* Text's string value */
 		std::string text;
+
+		/* Font the text renders in */
 		Font* font;
-		SDL_Colour colour;
+
+		/* Colour the text renders in */
+		Colour colour;
+
+		/* Window the text renders to */
 		Window* window;
+
+		/* Text's point of origin for rendering and rotations */
+		Vector2f origin;
 
 	};
 
