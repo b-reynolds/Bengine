d7ab8cff6623f808db90864eeaeaf10026eb10c7
Ben Reynolds <benr420@gmail.com>
2017-03-31 15:25:01 +0100

    Add comments to Scene Manager class

--- Bengine/Bengine/scene_manager.cpp
+++ Bengine/Bengine/scene_manager.cpp
@@ -1,12 +1,38 @@
 #include "scene_manager.h"
 #include "scene.h"
 
+/**
+ * \brief Add a scene to the Scene Manager
+ * \param scene_name scene's name
+ * \param scene scene
+ */
 void BG::SceneManager::add_scene(const std::string& scene_name, Scene& scene)
 {
 	scenes_.insert(std::pair<std::string, Scene*>(scene_name, &scene));
 }
 
-bool BG::SceneManager::set_current_scene(const std::string& scene_name)
+/**
+ * \brief Remove a scene from the Scene Manager
+ * \param scene_name scene's name
+ * \return removal successful
+ */
+bool BG::SceneManager::remove_scene(const std::string& scene_name)
+{
+	auto result = scenes_.find(scene_name);
+	if(result != scenes_.end())
+	{
+		scenes_.erase(result);
+		return true;
+	}
+	return false;
+}
+
+/**
+ * \brief Transition to another scene
+ * \param scene_name scene's name
+ * \return transition successful
+ */
+bool BG::SceneManager::transition_to(const std::string& scene_name)
 {
 	if(current_scene_.second != nullptr)
 	{
@@ -29,11 +55,17 @@ bool BG::SceneManager::set_current_scene(const std::string& scene_name)
 	return current_scene_.second != nullptr;
 }
 
+/**
+ * \brief Returns a reference to the current scene
+ */
 BG::Scene& BG::SceneManager::current_scene() const
 {
 	return *current_scene_.second;
 }
 
+/**
+ * \brief Returns the current scene's name
+ */
 std::string BG::SceneManager::current_scene_name() const
 {
 	return current_scene_.first;
--- Bengine/Bengine/scene_manager.h
+++ Bengine/Bengine/scene_manager.h
@@ -3,22 +3,38 @@
 
 namespace BG
 {
+
+	/* Forward declerations */
 	class Scene;
 
+	/*
+	 * \brief Scene Manager class that handles transitioning between scenes
+	 */
 	class SceneManager
 	{
 
 	public:
 
+		/* Add a scene to the Scene Manager */
 		void add_scene(const std::string& scene_name, Scene& scene);
-		bool set_current_scene(const std::string& scene_name);
 
+		bool remove_scene(const std::string& scene_name);
+
+		/* Transition to another scene */
+		bool transition_to(const std::string& scene_name);
+
+		/* Returns a reference to the current scene */
 		Scene& current_scene() const;
+
+		/* Returns the current scene's name */
 		std::string current_scene_name() const;
 
 	private:
 
+		/* Map of scenes */
 		std::map<std::string, Scene*> scenes_;
+
+		/* Currenty active scene */
 		std::pair<std::string, Scene*> current_scene_;
 
 	};
